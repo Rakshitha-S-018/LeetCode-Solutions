d73c9f68f801b293a7d8e020be4633b0cec197c8
Rakshitha-S-018 <rakshithayadav295@gmail.com>
2026-06-21 22:35:34 +0530

    Time: 0 ms (100%), Space: 19.3 MB (25.33%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isSubsequence(self, s, t):
+        i=0
+        j=0
+        while i< len(s) and j<len(t):
+            if s[i]==t[j]:
+                i+=1
+            j+=1
+        if i==len(s): #number of characters from s that were matched.
+            return True
+        else:
+            return False
+        
